a3d92c3001663fbceaa45933f8c2246a33f5c45e
Dennis Vinterfjärd <dennis@vinterfjard.com>
2024-02-05 11:24:53 +0100

    Update App.tsx
    Updating app.tsx to be able to run locally by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { io } from "socket.io-client";
 
-const CHAT_SERVER_URL = ""
+const CHAT_SERVER_URL = "127.0.0.1:3000" //default local port
 
 const connectChatServer = () => {
-  const socket = io(CHAT_SERVER_URL, {
-    transports: ["websocket"],
-    path: "/"
-  });
+  const socket = io(CHAT_SERVER_URL);
   socket.onAny((type: string, message: string, user: any) => console.log(type, message, user));
   return socket;
 };
